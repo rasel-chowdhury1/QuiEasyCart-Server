5aacde0b778c0e4a5c826c0ebc2b4c2925f639be
hridoythebest <hridoythebest@gmail.com>
2024-01-17 18:15:09 +0600

    code updated

--- index.js
+++ index.js
@@ -69,6 +69,14 @@ async function run() {
         res.send(result)
       })
 
+       //specific user get for admin
+       app.get('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id) }
+        const result = await useProfileCollection.findOne(query)
+        res.send(result)
+      })
+
 
         //update Profile 
     app.patch('/updateProfile/:id', async (req, res) => {
